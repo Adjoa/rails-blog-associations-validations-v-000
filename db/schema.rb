4a778c24f87807eda4c824ab058f13e91c828322
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-13 20:47:29 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180313204108) do
+ActiveRecord::Schema.define(version: 20180313204605) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20180313204108) do
     t.datetime "updated_at", null: false
     t.text     "content"
     t.integer  "user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
